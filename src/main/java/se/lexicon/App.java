d9b26aa4f0976cd12ce2f5a8c7730be99eec6ee3
Halamohamed <halaalimohammed@gmail.com>
2025-12-29 13:12:41 +0100

    add product to purchase

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -8,7 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        IVendingMachine vendingMachine = new VendingMachineImpl();
+        VendingMachineImpl vendingMachine = new VendingMachineImpl();
+        vendingMachine.addProduct(new Snack(1, "Chips", 15, 10));
+        vendingMachine.addProduct(new Beverage(2, "Soda", 10, 20));
+        vendingMachine.addProduct(new Snack(3, "Chocolate", 20, 5));
+        vendingMachine.addProduct(new Beverage(4, "Water", 5, 30));
+        vendingMachine.addProduct(new Beverage(5, "Juice", 12, 15));
+        vendingMachine.addProduct(new Fruit(6, "Apple", 8, 25));
+        vendingMachine.addProduct(new Fruit(7, "Banana", 6, 30));
         ConsoleUI consoleUI = new ConsoleUI(vendingMachine);
         consoleUI.start();
 
